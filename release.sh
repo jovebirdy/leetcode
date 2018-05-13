4780b7bdae74802c09bb1e5e09c31ceaffd14fbb
fakeYanss <yanshisangc@gmail.com>
2018-05-13 16:21:57 +0800

    add my solutions

--- release.sh
+++ release.sh
@@ -1,3 +1,9 @@
-git add .
-git commit -m "add submissions"
+cd solutions/ && \
+lc-spider -c config.json && \
+cd .. && \
+npm run generate && \
+rm -f README.md && \
+cp ./solutions/README.md ./ && \
+git add . && \
+git commit -m "add my solutions" && \
 git push origin master
